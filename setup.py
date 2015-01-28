ad767b1dc1b4476301ead9342784653de70cb47b
ondmagi <andreas@superblock.se>
2015-01-28 12:47:39 +0100

    modify setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open('README.rst', 'r', 'utf-8') as f:
+with open('README.rst', 'r') as f:
     readme = f.read()
 
 packages = [
@@ -28,9 +28,13 @@ setup(
     package_dir={'pybpdbjobs': 'pybpdbjobs'},
     requires=requires,
     license=pybpdbjobs.__license__,
+    keywords=['netbackup', 'bpdbjobs'],
     classifiers=(
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Natural Language :: English',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
     )
 )
